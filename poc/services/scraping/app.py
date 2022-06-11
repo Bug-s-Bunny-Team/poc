5e67d805896b4a7df0f2df81bf8aa9d2afa061eb
Marco Volpato <marco@mvolpato.xyz>
2022-06-11 11:05:51 +0200

    deploy test

--- poc/services/scraping/app.py
+++ poc/services/scraping/app.py
@@ -2,9 +2,10 @@ import json
 
 
 def lambda_handler(event, context):
+    message = event.get('message')
     return {
         'statusCode': 200,
         'body': json.dumps({
-            'message': 'hello from ScrapingService',
+            'message': message,
         }),
     }
