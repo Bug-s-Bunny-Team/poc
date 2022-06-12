3f2d6fa72b911be6af6b3266e62809a47444dfc0
Marco Volpato <marco@mvolpato.xyz>
2022-06-12 17:00:46 +0200

    import instaloader session from env var #5

--- poc/services/scraping/models.py
+++ poc/services/scraping/models.py
@@ -1,16 +1,18 @@
 from dataclasses import dataclass
-from enum import Enum, unique
+from enum import IntEnum, unique
 from typing import Set
+
+from instaloader import Post as InstaPost
 
 
 @unique
-class Social(Enum):
+class Social(IntEnum):
     TIKTOK = 0
     INSTAGRAM = 1
 
 
 @unique
-class MediaType(Enum):
+class MediaType(IntEnum):
     IMAGE = 0
     VIDEO = 1
 
@@ -27,3 +29,13 @@ class Post:
     def hashtags(self) -> Set[str]:
         tags = [tag.strip('#') for tag in self.caption.split() if tag.startswith('#')]
         return set(tags)
+
+    @classmethod
+    def from_instaloader_post(cls, post: InstaPost):
+        return cls(
+            id=post.shortcode,
+            caption=post.caption,
+            media_url=post.video_url if post.is_video else post.url,
+            social=Social.INSTAGRAM,
+            media_type=MediaType.VIDEO if post.is_video else MediaType.IMAGE
+        )
