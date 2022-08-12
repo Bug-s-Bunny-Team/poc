fdbd28998e9f57790642c558b15bed1a63b0e2c5
giuliozanatta <62381112+giuliozanatta@users.noreply.github.com>
2022-08-12 12:20:02 +0200

    Add sample of Login + Register

---
+++ poc/frontend/src/pages/Register.svelte
@@ -0,0 +1,50 @@
+<script>
+
+    import { Navigate } from 'svelte-router-spa'
+	let email;
+    let password;
+    let firstName;
+    let lastName;
+    let loading;
+
+
+    export let sdkoptions;
+
+    const handleSubmit=()=>{
+        const signupFields={
+            "FirstName": firstName,
+            "LastName": lastName,
+            "Email": [
+                {
+                "Type": "Primary",
+                "Value": email
+                }
+            ],
+            "Password": password
+     }
+     console.log(signupFields)
+   }
+
+</script>
+ 
+ <h2>Register</h2>
+    <form>
+        <div class="inputBox">
+            <label for="userName">Username</label>
+            <input type="text" name="userName" id="userName" placeholder="type your username" required/>
+        </div>
+        <div class="inputBox">
+            <label for="userPassword">Password</label>
+            <input type="password" name="userPassword" id="userPassword" placeholder="type your password"
+                   required/>
+        </div>
+        <div class="inputBox">
+            <label for="userConfirmPassword">Confirm Password</label>
+            <input type="password" name="userPassword" id="userConfirmPassword" placeholder="confirm your password"
+                   required/>
+        </div>
+        <button type="submit" name="" style="float: left;">Submit</button>
+        <strong class="link"><Navigate to="/login">Login</Navigate></strong>
+    </form>
+<footer>
+</footer>
--- poc/frontend/src/routes.ts
+++ poc/frontend/src/routes.ts
@@ -1,14 +1,29 @@
 import MainLayout from "./components/MainLayout.svelte";
 import Scrape from "./pages/Scrape.svelte";
 import Posts from "./pages/Posts.svelte";
+import Login from "./pages/Login.svelte";
+import Register from "./pages/Register.svelte";
 
 const routes = [
+    {
+        name: '/login',
+        title: 'Login',
+        component: Login,
+        layout: MainLayout
+    },
+    {
+        name: '/register',
+        title: 'Register',
+        component: Register,
+        layout: MainLayout
+    },
     {
         name: '/',
         title: 'Scrape',
         component: Scrape,
         layout: MainLayout
     },
+    
     {
         name: '/posts',
         title: 'Posts',
