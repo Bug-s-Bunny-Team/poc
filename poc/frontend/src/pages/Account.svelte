d28d253b124319776984f33bc4739006b684d100
Tommaso Di Fant <jackiedifant@gmail.com>
2022-08-13 14:42:03 +0200

    .

--- poc/frontend/src/pages/Account.svelte
+++ poc/frontend/src/pages/Account.svelte
@@ -6,13 +6,31 @@
 
 <h2>My Account</h2>
 <h3> Your personal information </h3>
+<article>
     <p> Name: { presenter.name }</p>
     <p> Email: { presenter.email }</p>
 
-    <label for="choosePreference" class="link">Choose your predefined guide: </label>
-    <input type="checkbox" name="preference" id="choosePreference" on:change={presenter.changePreference} bind:checked={presenter.preference} >
+    <label for="choosePreference" class="link">Display my guide as map: </label>
+    <input type="checkbox" name="preference" id="choosePreference" role="switch" on:change={presenter.changePreference} bind:checked={presenter.preference} >
 
-    <strong class="link"><Navigate to="/changepsw">Change your password </Navigate></strong> <br>
-    <button type="submit" name="" style="float: left;">Logout</button>
+    <strong class="link"><Navigate to="/changepsw">Change your password </Navigate></strong>
+    <button type="submit" name="" id="logout">Logout</button>
+</article>
 <footer>
 </footer>
+
+<style>
+
+    #choosePreference {
+        display: block;
+        margin-top: 0.5em;
+        margin-bottom: 1.5em;
+        
+    }
+
+    strong.link {
+        display: block;
+        margin-bottom: 1.5em;
+    }
+
+</style>
